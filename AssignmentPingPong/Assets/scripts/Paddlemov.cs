1fcedabd8d9acfdbbcca03bb513a0c66b93a2bdf
Julian Calleja <Julian Calleja>
2017-12-05 22:52:48 +0100

    Assignment check movement problem

---
+++ AssignmentPingPong/Assets/scripts/Paddlemov.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddlemov : MonoBehaviour {
+    public float move = 30;
+    
+
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+    
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(moveUp))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, move, 0);
+        }
+
+        if (Input.GetKeyDown(moveDown))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, -move, 0);
+        }
+        
+	}
+
+    private void FixedUpdate()
+    {
+      
+        
+
+    }
+}
