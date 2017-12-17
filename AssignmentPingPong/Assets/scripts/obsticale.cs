5e5fea5dc89af46b45d02c257d905f3636be62d3
Julian Calleja <Julian Calleja>
2017-12-17 16:55:19 +0100

    assignment check 9: obsticales (level3 implemented

---
+++ AssignmentPingPong/Assets/scripts/obsticale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obsticale : MonoBehaviour {
+    float movespeed = 1f;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        movespeed += (Time.deltaTime * 0.5f);
+        
+
+        transform.Rotate(0,0, movespeed);
+
+
+
+    }
+}
